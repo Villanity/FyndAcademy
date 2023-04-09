e0e3978643dad08a34c32e99585cb26e7a28bc42
villanity <villanity@Villanitys-MacBook-Air.local>
2023-04-09 22:57:59 +0530

    JS Porject

--- Projects/JS/script.js
+++ Projects/JS/script.js
@@ -1,11 +1,9 @@
-const header = document.querySelector('.navbar');
+function openNav(){
+    document.getElementById("myNav")
+    .style.height="100%";
+}
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
+function closeNav(){
+    document.getElementById("myNav")
+    .style.height="0%";
 }
