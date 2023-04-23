0bab5263c3809e2be5e3a3fa0c7db9544e5406ed
Villanity <assassinghostcsgo@gmail.com>
2023-04-23 20:42:39 +0530

    .once

--- Assignments/form-shop/src/components/QuizPage.vue
+++ Assignments/form-shop/src/components/QuizPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="content">
-      <h2>{{ question }}</h2>
+    <center>
+      <h2 class="question">{{ question }}</h2>
+    </center>
+      
       <ul>
 
         <!-- <p v-for="answer in answers"
@@ -16,7 +19,6 @@
     </p> -->
 
         <div v-for="answer in answers" :key="answer">
-
           <!-- <p :class="{
           'correct': selectedAnswer === correctAnswer,
           'incorrect': selectedAnswer !== correctAnswer && selectedAnswer !== null}" class="options" @click="selectedAnswer = answer, this.submitDisabled = false">{{ answer }}</p> -->
@@ -25,14 +27,17 @@
               <p  :class="{
               'correct': selectedAnswer === correctAnswer,
               'incorrect': selectedAnswer !== correctAnswer && selectedAnswer !== null
-            }" class="options" @click="checkAnswer(answer, $event)" :disabled="selectedAnswer === answer">{{ answer }}</p>
+            }" class="options" @click.once="checkAnswer(answer, $event)" :disabled="selectedAnswer === answer">{{ answer }}</p>
 
         </div>
-        <button class="btn btn-primary" id="submitAnswer" @click="loadQuestion()" :disabled="submitDisabled">Submit
+        <button class="submitAnswer mt-2" id="submitAnswer" @click="loadQuestion()" :disabled="submitDisabled">Submit
           Answer</button>
       </ul>
+      <center>
+        You Scored {{ score }} / {{ lengthData }} with {{ wrongScore }} wrong answers. 
 
-      You Scored {{ score }} / {{ lengthData }} with {{ wrongScore }} wrong answers. 
+      </center>
+
 
   </div>
 </template>
@@ -71,6 +76,7 @@ export default {
           this.correctAnswer = quizData.correctAnswer;
           this.submitDisabled = true;
           this.lengthData = data.length;
+          this.clickCounter = 0;
           if (this.i != data.length - 1) {
             this.i += 1;
           }
@@ -90,7 +96,7 @@ export default {
     // }
 
     checkAnswer(answer, event) {
-      const answerElement = event.target;
+        const answerElement = event.target;
 
       if (answer === this.correctAnswer) {
         answerElement.classList.add('correct');
@@ -99,9 +105,10 @@ export default {
         answerElement.classList.add('incorrect');
         this.wrongScore += 1;
       }
-
+    
       this.selectedAnswer = null;
       this.submitDisabled = false;
+      
     },
 
   },
@@ -109,33 +116,101 @@ export default {
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+
+* {
+    font-family: 'Lato', sans-serif;
+}
+
 body {
-  background-color: rgb(28, 0, 46);
-  color: white;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: 300% 300%;
+  background-image: linear-gradient(-45deg, yellow 0%, yellow 25%, yellow 51%, #ff357f 100%);
+  -webkit-animation: AnimateBG 20s ease infinite;
+          animation: AnimateBG 20s ease infinite;
+}
+
+@-webkit-keyframes AnimateBG {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes AnimateBG {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+.question {
+  padding: 40px;
+  /* width: auto; */
+  font-weight: 700;
+  /* margin-left: 25px; */
 }
 
 .content {
-  max-width: 500px;
   margin: auto;
 }
 
 .options {
-  background-color: brown;
+  max-width: 600px;
+  /* background: rgb(131,58,180); */
+  background: radial-gradient(circle, rgba(63,185,251,1) 0%, rgba(70,90,252,1) 100%);
   padding: 20px;
   color: white;
   cursor: pointer;
-  border-radius: 10px;
+  border-radius: 5px;
 }
-
 .options:hover {
   background-color: rgb(69, 42, 165);
+  background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
 }
 
 .correct {
-  background-color: rgb(0, 162, 19);
+  /* background-color: rgb(0, 162, 19); */
+  background: linear-gradient(90deg, rgba(58,180,137,1) 0%, rgba(29,253,87,1) 50%, rgba(105,252,69,1) 100%);
 }
 
 .incorrect {
-  background-color: rgb(255, 0, 0);
+  /* background-color: rgb(255, 0, 0); */
+  background: linear-gradient(90deg, rgba(180,58,58,1) 0%, rgba(253,29,29,1) 50%, rgba(252,69,69,1) 100%);
+}
+
+.submitAnswer:disabled {
+  /* background-color: #ff357f; */
+  background: linear-gradient(90deg, rgb(113, 113, 113) 0%, rgb(107, 107, 107) 50%, rgb(94, 94, 94) 100%);
+  padding: 15px;
+  border-radius: 5px;
+  color: rgb(255, 255, 255);
+  border: none;
+  cursor:crosshair;
+}
+
+.submitAnswer:enabled {
+  /* background-color: #4935ff; */
+  background: linear-gradient(90deg, rgba(58,180,137,1) 0%, rgba(29,253,87,1) 50%, rgba(105,252,69,1) 100%);
+  padding: 15px;
+  border-radius: 5px;
+  color: rgb(0, 0, 0);
+  border: none;
+  cursor: pointer;
 }
 </style>
