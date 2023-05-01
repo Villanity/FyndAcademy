4b4ca641fcfa42fd5b0b69b962d400d17bf6a876
Villanity <assassinghostcsgo@gmail.com>
2023-05-01 19:25:24 +0530

    Input Sources Added

--- Projects/Data-Visualization/datavisualization/src/components/dashboardPage.vue
+++ Projects/Data-Visualization/datavisualization/src/components/dashboardPage.vue
@@ -1,43 +1,117 @@
 <template>
-  <h3>dashboardPage</h3>
-  <canvas ref="chartCanvas"></canvas>
+  <div class="col-md-4">
+    <div class="col">
+      <div class="dataImport">
+        <h2>Please choose your Data Source :</h2>
+        <div class="importOptions">
+          <select name="import-source" id="import-source" v-model="importSource">
+            <option value="none" selected disabled>Select an Import Source</option>
+            <option value="json">JSON</option>
+            <option value="api">API</option>
+          </select>
+        </div>
+
+        <div class="nextAction" id="nextAction">
+          <div v-if="importSource === 'json'">
+            <input type="file" @change="importJSON">
+          </div>
+          <div v-else-if="importSource === 'api'">
+            <label for="APIField">Please enter the URL</label>
+            <input type="text" name="APIField" id="APIField" v-model="apiLink">
+            <button @click="importFromAPI(apiLink)"> Fetch </button>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="showType != false">
+        <div class="chartType">
+          <select name="chart-type" id="chart-type" v-model="chartType" @change="setType">
+            <option value="bar">Bar</option>
+            <option value="line">Line</option>
+            <option value="bubble">Bubble</option>
+            <option value="doughnut">Doughnut</option>
+            <option value="pie">Pie</option>
+            <option value="polarArea">Polar Area</option>
+            <option value="radar">Radar</option>
+            <option value="scatter">Scatter</option>
+          </select>
+        </div>
+      </div>
+
+      <canvas ref="chartCanvas"></canvas>
+
+    </div>
+  </div>
 </template>
 <script>
 import Chart from 'chart.js/auto'
 
 export default {
   name: 'dashboardPage',
-  mounted() {
-    this.createChart();
+  data() {
+    return {
+      importSource: "none",
+      chartType: "bar",
+      showType: false,
+      type: 'bar',
+      chartInstance: null
+    }
   },
   methods: {
-    createChart() {
-      const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-      ];
-      const ctx = this.$refs.chartCanvas.getContext('2d');
-      new Chart(
-        ctx,
-        {
-          type: 'bar',
-          data: {
-            labels: data.map(row => row.year),
-            datasets: [
-              {
-                label: 'Acquisitions by year',
-                data: data.map(row => row.count)
-              }
-            ]
-          }
+
+    importJSON(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const data = JSON.parse(e.target.result);
+          this.$emit("dataImported", data);
+          this.createChart(data);
+          console.log("JSON Loaded", data);
+        };
+        reader.readAsText(file);
+      }
+    },
+    async importFromAPI(apiURL) {
+      // apiURL = 'https://api.npoint.io/8f97cfcef94b992f44f6';
+      try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        this.$emit("dataImported", data);
+        this.createChart(Array.from(data));
+        console.log("Data Loaded from API", data);
+      } catch (error) {
+        console.error("Error fetching data from API:", error);
+      }
+    },
+
+    setType() {
+      this.type = this.chartType;
+      this.createChart();
+    },
+
+    createChart(chartData) {
+      this.showType = true;
+
+      const canvas = this.$refs.chartCanvas;
+      const ctx = canvas.getContext('2d');
+
+      if (this.chartInstance != null) {
+        this.chartInstance.destroy();
+      }
+
+      this.chartInstance = new Chart(ctx, {
+        type: this.type,
+        data: {
+          labels: chartData.map(row => row.year),
+          datasets: [{
+            label: 'Acquisitions by year',
+            data: chartData.map(row => row.count)
+          }]
         }
-      );
+      });
     }
+
   }
 }
 </script>
