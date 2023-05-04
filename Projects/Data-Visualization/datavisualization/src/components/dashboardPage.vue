e73011c06c99dc8f48ad86d21fc8d2c50755c400
Villanity <assassinghostcsgo@gmail.com>
2023-05-04 14:48:52 +0530

    Added CSV and Excel Import

--- Projects/Data-Visualization/datavisualization/src/components/dashboardPage.vue
+++ Projects/Data-Visualization/datavisualization/src/components/dashboardPage.vue
@@ -6,14 +6,22 @@
         <div class="importOptions">
           <select name="import-source" id="import-source" v-model="importSource">
             <option value="none" selected disabled>Select an Import Source</option>
+            <!-- <option value="xlsx">Excel</option> -->
+            <option value="csv">EXCEL or CSV</option>
             <option value="json">JSON</option>
             <option value="api">API</option>
           </select>
         </div>
 
         <div class="nextAction" id="nextAction">
-          <div v-if="importSource === 'json'">
-            <input type="file" @change="importJSON">
+          <!-- <div v-if="importSource === 'xlsx'">
+            <input type="file" accept=".xlsx, .xls" @change="importExcel">
+          </div> -->
+          <div v-if="importSource === 'csv'">
+            <input type="file" accept=".csv, .xlsx, .xls" @change="importCSV">
+          </div>
+          <div v-else-if="importSource === 'json'">
+            <input type="file" accept=".json" @change="importJSON">
           </div>
           <div v-else-if="importSource === 'api'">
             <label for="APIField">Please enter the URL</label>
@@ -44,7 +52,9 @@
   </div>
 </template>
 <script>
-import Chart from 'chart.js/auto'
+import Chart from 'chart.js/auto';
+import Papa from 'papaparse';
+import ExcelJS from 'exceljs';
 
 export default {
   name: 'dashboardPage',
@@ -56,25 +66,81 @@ export default {
       type: 'bar',
       chartInstance: null,
       jsonEvent: null,
+      csvEvent: null,
       apiURLType: '',
     }
   },
   methods: {
 
+    importCSV(event) {
+      this.csvEvent = event;
+      const file = event.target.files[0];
+
+      // Convert Excel files to CSV format
+      if (file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const buffer = reader.result;
+          const workbook = new ExcelJS.Workbook();
+          workbook.xlsx.load(buffer).then(() => {
+            const worksheet = workbook.getWorksheet(1);
+            const csvData = [];
+
+            // Convert each row to CSV format
+            worksheet.eachRow({ includeEmpty: false }, (row) => {
+              const csvRow = row.values.join(',');
+              csvData.push(csvRow);
+            });
+
+            // Join all rows together into a single CSV string
+            const csvString = csvData.join('\n');
+
+            // Parse the CSV string using Papa.parse
+            const parsedData = Papa.parse(csvString, { header: true }).data;
+            this.createChart(parsedData);
+            this.$emit('dataImported', parsedData);
+          });
+        };
+        reader.readAsArrayBuffer(file);
+      }
+
+      // Handle regular CSV files
+      else if (file.type === 'text/csv') {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const data = Papa.parse(reader.result, { header: true }).data;
+          this.createChart(data);
+          this.$emit('dataImported', data);
+        };
+        reader.readAsText(file);
+      }
+
+      else {
+        alert('Error: Please upload a CSV file.');
+        return;
+      }
+    },
+
     importJSON(event) {
+      // console.log(event)
       this.jsonEvent = event;
       const file = event.target.files[0];
       if (file) {
         const reader = new FileReader();
         reader.onload = (e) => {
-          const data = JSON.parse(e.target.result);
-          this.$emit("dataImported", data);
-          this.createChart(data);
-          console.log("JSON Loaded", data);
+          try {
+            const data = JSON.parse(e.target.result);
+            this.$emit("dataImported", data);
+            this.createChart(data);
+            console.log("JSON Loaded", data);
+          } catch (error) {
+            alert("Error: Invalid JSON format.");
+          }
         };
         reader.readAsText(file);
       }
     },
+
     async importFromAPI(apiURL) {
       this.apiURLType = apiURL;
       // apiURL = 'https://api.npoint.io/8f97cfcef94b992f44f6';
@@ -93,10 +159,13 @@ export default {
       this.type = this.chartType;
       const chartData = this.chartInstance.config.data;
       this.createChart(chartData);
+      if (this.importSource === 'csv') {
+        this.importCSV(this.csvEvent);
+      }
       if (this.importSource === 'json') {
         this.importJSON(this.jsonEvent);
       }
-      else {
+      if (this.importSource === 'api') {
         this.importFromAPI(this.apiURLType);
       }
     },
@@ -109,8 +178,6 @@ export default {
 
       this.showType = true;
       const typeChart = this.type;
-      console.log(typeChart);
-      console.log(chartData);
 
       const canvas = this.$refs.chartCanvas;
       const ctx = canvas.getContext('2d');
